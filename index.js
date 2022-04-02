1b3d8036f8acd5dfaf72a16dfcdb3d8e3c5422f7
Aditya Garg <adityagarg2198@gmail.com>
2022-04-02 16:30:46 +0530

    mark feature added

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-const todo=document.querySelector('.todo-input');
-const addButton=document.querySelector('.add-button');
-const list=document.querySelector('.list');
+const todo = document.querySelector(".todo-input");
+const addButton = document.querySelector(".add-button");
+const list = document.querySelector(".list");
 let todoValue;
 
 //getting the todo value
-todo.addEventListener('change',(e)=>{
-    todoValue=todo.value;
-})
-
+todo.addEventListener("change", (e) => {
+  todoValue = todo.value;
+});
 
 //adding the todo in the list
-addButton.addEventListener('click',(e)=>{
-    todo.value='';
-    e.preventDefault();
-    const item=document.createElement('li');
-    item.textContent=todoValue;
-    item.className='todo-item';
-    list.appendChild(item);
-})
+addButton.addEventListener("click", (e) => {
+  todo.value = "";
+  e.preventDefault();
+  const listitem = document.createElement("li");
+  const button = document.createElement("button");
+  button.textContent = "mark";
+  button.className = "mark-btn";
+  listitem.textContent = todoValue;
+  listitem.appendChild(button);
+  listitem.className = "todo-item";
+  list.appendChild(listitem);
+  markfunc();
+});
+
+function markfunc() {
+  const btns = document.querySelectorAll(".mark-btn");
+  btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      btn.parentElement.classList.toggle('mark');
+    });
+  });
+}
