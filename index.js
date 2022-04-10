d4ad2f09afae70ba3348149110f060e4113bd3c4
Aditya Garg <adityagarg2198@gmail.com>
2022-04-10 22:36:16 +0530

    delete feature

--- index.js
+++ index.js
@@ -1,33 +1,87 @@
-const todo = document.querySelector(".todo-input");
 const addButton = document.querySelector(".add-button");
 const list = document.querySelector(".list");
-let todoValue;
-
-//getting the todo value
-todo.addEventListener("change", (e) => {
-  todoValue = todo.value;
-});
 
 //adding the todo in the list
 addButton.addEventListener("click", (e) => {
-  todo.value = "";
   e.preventDefault();
-  const listitem = document.createElement("li");
-  const button = document.createElement("button");
-  button.textContent = "mark";
-  button.className = "mark-btn";
-  listitem.textContent = todoValue;
-  listitem.appendChild(button);
-  listitem.className = "todo-item";
-  list.appendChild(listitem);
+  const todoValue = document.querySelector(".todo-input").value;
+  todo.value = "";
+  storeTodos(todoValue);
   markfunc();
+  deletefunc();
 });
+
+function storeTodos(newTodo) {
+  const todo = {
+    content: newTodo,
+    toggler: false,
+  };
+  if (localStorage.getItem("todos") === null) {
+    let data = [];
+    data.push(todo);
+    localStorage.setItem("todos", JSON.stringify(data));
+  } else {
+    let data = JSON.parse(localStorage.getItem("todos"));
+    data = [...data, todo];
+    localStorage.setItem("todos", JSON.stringify(data));
+  }
+}
+
+function renderList(data) {
+  const listitem = document.createElement("li");
+  const markButton = document.createElement("button");
+  const deleteButton = document.createElement("button");
+
+  markButton.textContent = "tick";
+  markButton.classList.add("btn");
+  markButton.classList.add("mark-btn");
+
+  deleteButton.textContent = "delete";
+  deleteButton.classList.add("btn");
+  deleteButton.classList.add("delete-btn");
+
+  listitem.textContent = data.content;
+  listitem.className = "todo-item";
+  if (data.toggler === false) {
+    listitem.classList.remove("mark");
+  } else if (data.toggler === true) {
+    listitem.classList.add("mark");
+  }
+  listitem.appendChild(markButton);
+  listitem.appendChild(deleteButton);
+  list.appendChild(listitem);
+}
+
+function display() {
+  const todos = JSON.parse(localStorage.getItem("todos"));
+  todos.forEach((data) => {
+    renderList(data);
+  });
+}
+display();
+
+function deletefunc() {
+  const btns = document.querySelectorAll(".delete-btn");
+  btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.target.parentElement.remove();
+    });
+  });
+}
 
 function markfunc() {
   const btns = document.querySelectorAll(".mark-btn");
   btns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
-      btn.parentElement.classList.toggle('mark');
+      const element = e.target.parentElement;
+      const data=element.firstChild.textContent;
+      if(element.classList("mark")){
+        element.classList.remove("mark");
+        let data = JSON.parse(localStorage.getItem("todos"));
+        data.includes({content:})
+      }else{
+        element.classList.add("mark");
+      }
     });
   });
 }
